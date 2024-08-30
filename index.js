e417bb15dee49234325abedc3fdf45de5ada0d83
Patrick <opatricktavares@gmail.com>
2024-08-30 11:27:34 -0300

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const getGoldPrice = async () => {
+  try {
+    const response = await axios.get('https://www.investing.com/commodities/gold');
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const goldPrice = $('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]').text();
+
+    return goldPrice;
+  } catch (error) {
+    console.error('Erro ao obter a cotação do ouro:', error);
+    throw error;
+  }
+};
+
+app.get('/gold-price', async (req, res) => {
+  try {
+    const price = await getGoldPrice();
+    res.json({ goldPrice: price });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao obter a cotação do ouro' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});

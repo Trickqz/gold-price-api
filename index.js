657a25602148c2d2ac3ea15d8292c6ea46b51fec
Patrick <opatricktavares@gmail.com>
2024-08-30 12:26:32 -0300

    fix puppeteer && fix cors

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const cors = require('cors'); // Importa o middleware CORS
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Usa o middleware CORS
+app.use(cors());
+
+// Função para fazer scraping da cotação do ouro usando Puppeteer
 const getGoldPrice = async () => {
   try {
-    const response = await axios.get('https://www.investing.com/commodities/gold');
-    const html = response.data;
-    const $ = cheerio.load(html);
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('https://www.investing.com/commodities/gold');
+    
+    // Aguarda o elemento que contém o preço
+    await page.waitForSelector('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]');
+    
+    // Captura o preço do ouro
+    const goldPrice = await page.$eval('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]', el => el.textContent);
+    console.log('Cotação obtida:', goldPrice); // Log para verificar o preço capturado
 
-    const goldPrice = $('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]').text();
-
+    await browser.close();
     return goldPrice;
   } catch (error) {
     console.error('Erro ao obter a cotação do ouro:', error);
@@ -20,6 +30,7 @@ const getGoldPrice = async () => {
   }
 };
 
+// Endpoint para retornar a cotação do ouro
 app.get('/gold-price', async (req, res) => {
   try {
     const price = await getGoldPrice();
@@ -29,6 +40,7 @@ app.get('/gold-price', async (req, res) => {
   }
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });

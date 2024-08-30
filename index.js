916801613e75e9c9b78234111c3043aba9e00e83
Patrick <opatricktavares@gmail.com>
2024-08-30 12:26:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const cors = require('cors'); // Importa o middleware CORS
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Usa o middleware CORS
 app.use(cors());
 
-// Função para fazer scraping da cotação do ouro usando Puppeteer
 const getGoldPrice = async () => {
   try {
     const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://www.investing.com/commodities/gold');
     
-    // Aguarda o elemento que contém o preço
     await page.waitForSelector('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]');
     
-    // Captura o preço do ouro
     const goldPrice = await page.$eval('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]', el => el.textContent);
-    console.log('Cotação obtida:', goldPrice); // Log para verificar o preço capturado
+    console.log('Cotação obtida:', goldPrice);
 
     await browser.close();
     return goldPrice;
@@ -30,7 +26,6 @@ const getGoldPrice = async () => {
   }
 };
 
-// Endpoint para retornar a cotação do ouro
 app.get('/gold-price', async (req, res) => {
   try {
     const price = await getGoldPrice();
@@ -40,7 +35,6 @@ app.get('/gold-price', async (req, res) => {
   }
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });

b48b5e07ef60d385242ed937a2ecb6cfbf0566cf
Patrick <opatricktavares@gmail.com>
2024-08-30 12:38:19 -0300

    fix error

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-
 const getGoldPrice = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto('https://www.investing.com/commodities/gold');
-    
-    await page.waitForSelector('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]');
-    
-    const goldPrice = await page.$eval('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]', el => el.textContent);
-    console.log('Cotação obtida:', goldPrice);
+    const response = await axios.get('https://www.investing.com/commodities/gold');
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-    await browser.close();
+    const goldPrice = $('.text-5xl\\/9.font-bold.text-\\[\\#232526\\].md\\:text-\\[42px\\].md\\:leading-\\[60px\\]').text();
+
     return goldPrice;
   } catch (error) {
     console.error('Erro ao obter a cotação do ouro:', error);
@@ -30,6 +24,7 @@ app.get('/gold-price', async (req, res) => {
   try {
     const price = await getGoldPrice();
     res.json({ goldPrice: price });
+    console.log("cotação: " + JSON.stringify(price));
   } catch (error) {
     res.status(500).json({ error: 'Erro ao obter a cotação do ouro' });
   }

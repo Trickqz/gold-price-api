8c9ccd7ce2813ad0ce5d60ea8bc7c36e65bc656f
Patrick Tavares <145362922+Trickqz@users.noreply.github.com>
2024-12-05 12:22:18 -0300

    redeploy

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
